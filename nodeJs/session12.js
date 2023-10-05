ff35ae52af3c32583fad820b30d21454c2c79b9e
omarN10 <142399828+omarN10@users.noreply.github.com>
2023-10-05 12:30:34 +0300

    Developer (#1)
    * modified:   nodeJs/session11.js
    
    * \	new file:   nodeJs/session12.js
    
    * modified:   nodeJs/session11.js
    
    * modified:   nodeJs/session11.js
    
    * modified:   nodeJs/session12.js
    
    ---------
    
    Co-authored-by: Omar <omar.nasr@qeema.net>

---
+++ nodeJs/session12.js
@@ -0,0 +1,9 @@
+//scopes and variables accessibility
+//can you access variables anywhere? no need to know the scope
+
+//global scope variable without any function scope or outside block of code
+
+//const is a variable cannot be changed after declaration
+
+//let, var, const
+//const number1 = 10;   //cannot be changed in the future
