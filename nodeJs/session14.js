c5dbe747ac22a9401826ba1175d9cd87c1c1d9bc
Omar <omar.nasr@qeema.net>
2023-10-05 13:48:33 +0300

    new file created = session14

--- nodeJs/session14.js
+++ nodeJs/session14.js
@@ -1,6 +1,21 @@
 {
 	//block scope
-	var data = 5;
+	// var data = 4;
 }
 
+// console.log(data);
+
+//any varialbe declared in function only accessible in the function scope
+function test() {
+	var data = 5;
+	console.log(data);
+}
+// test();
+
+function test1() {
+	var data = 5;
+	console.log(data);
+}
+test1();
+var data = 14;
 console.log(data);
