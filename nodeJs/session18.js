9ffa80d5abb6cb429499b18d829dc6264de93337
omarN10 <142399828+omarN10@users.noreply.github.com>
2023-10-08 14:20:06 +0300

    test (#3)
    * modified:   nodeJs/session11.js
    
    * \	new file:   nodeJs/session12.js
    
    * modified:   nodeJs/session11.js
    
    * modified:   nodeJs/session11.js
    
    * modified:   nodeJs/session12.js
    
    * modified:   nodeJs/session12.js
    	new file:   nodeJs/session13.js
    
    * modified:   nodeJs/session13.js
    
    * new file created
    
    * new file created = session14
    
    * Update session14.js
    
    * new file:   nodeJs/session15.js
    
    * new file:   nodeJs/session16.js
    	new file:   nodeJs/session17.js
    
    * modified:   nodeJs/session17.js
    
    * new file:   nodeJs/session18.js
    
    * modified:   nodeJs/session18.js
    
    * modified:   testing.js
    
    ---------
    
    Co-authored-by: Omar <omar.nasr@qeema.net>

---
+++ nodeJs/session18.js
@@ -0,0 +1,34 @@
+//hands on exercises
+
+//get the factorial !5
+
+let number = 10,
+	total = 1;
+let iteration = number;
+// for (let i = 0; i <= 5; i++) {
+// 	if (number == 0) {
+// 		break;
+// 	}
+// 	console.log((total *= number));
+// 	number--;
+// }
+
+// for (let i = 0; i < iteration; i++) {
+// 	// total +=  number * number-1;
+// 	// console.log(total);
+// 	// console.log(number * (number - 1));
+// 	total *= number;
+// 	number--;
+// 	console.log(total);
+// }
+
+// for (let i = 2; i <= number; i++) {
+// 	total = total * i;
+// 	console.log(total);
+// }
+// console.log(total);
+
+
+//prime number from  1 to n
+//input = n
+
