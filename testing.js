bdefab341a9f551c55e0a68b234219752cea9202
Omar <omar.nasr@qeema.net>
2023-10-08 15:08:32 +0300

    modified:   testing.js

--- testing.js
+++ testing.js
@@ -36,12 +36,27 @@ const codeBlocker = async () => {
 	return Promise.resolve().then(() => {
 		let i = 0;
 		while (i < 10) {
-            console.log("omar");
+			console.log("omar");
 			i++;
 		}
 		return "count to 10k is complete";
 	});
 };
-console.log("test a");
-codeBlocker().then(x => console.log(x));
-console.log("test b");
+// console.log("test a");
+// codeBlocker().then(x => console.log(x));
+// console.log("test b");
+
+//testing retrieving data from json file
+
+//constructor function example
+
+let person = function (name, age, id) {
+	this.name = name;
+	this.age = age;
+	this.id = id;
+};
+let person1 = new person("omar", 27, 10);
+
+console.log(
+	`person1 name is ${person1.name}, age is ${person1.age} and id is ${person1.id}`,
+);
