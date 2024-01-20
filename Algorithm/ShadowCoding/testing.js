4585bc7008f957461f1c1e188ee8f8e3bd42b916
OmarNasrOwis <ramorsan2013@gmail.com>
2024-01-20 09:22:43 +0200

    new file:   Algorithm/ShadowCoding/bubblesort.js

--- Algorithm/ShadowCoding/testing.js
+++ Algorithm/ShadowCoding/testing.js
@@ -34,8 +34,43 @@ function fact(number) {
   //recursive case
   return number * fact(number - 1);
 }
- 
-console.log(fact(5));
+
+/* console.log(fact(5));
 console.log(fact(6));
 console.log(fact(-10));
+ */
+//linear search vs binary search
+let testingArr = [1, 4, 6, 8, 9, 2];
+let sortArr = testingArr.sort((a, b) => a - b);
+console.log(sortArr);
+function linearSearch(numbers, n) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] == n) {
+      return i;
+    }
+  }
+  return -1;
+}
+// console.log(linearSearch(testingArr,9));
 
+function binarySearch(number, n) {
+  //check if array is not empty
+  if (number.length < 1) {
+    return -1;
+  }
+  //create left right and mid
+  let left = 0;
+  let right = number.length - 1;
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (number[mid] == n) {
+      return mid;
+    } else if (number[mid] > n) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+}
+
+console.log(binarySearch(sortArr, 1));
